7b1ca5b6bbdad60efc8ff2f54a18be74831a0afe
Fawad Mahdi <fawad@Fawads-MacBook-Pro.local>
2024-11-17 23:02:09 +0500

    Modify app.py to show Hello Docker

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-# app.py
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+
+@app.route('/')
+def home():
+    # Display a simple message for the root route
+    return "<h1>Hello World Docker</h1>"
 
 @app.route('/add', methods=['GET'])
 def add():
